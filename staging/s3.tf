742b3dcace202f25b5a5e3ad999113a95cd5dd80
inamuu <kzm0211@gmail.com>
2018-10-26 20:40:50 +0900

    :sparles: S3をWebホスティングする書き方

---
+++ staging/s3.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "website_hosting_inamuu_com" {
+  bucket = "website-hosting.inamuu.com"
+  acl    = "public-read"
+  policy = "${file("files/s3/policy/website_hosting_inamuu.com.json")}"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}

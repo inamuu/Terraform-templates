1970e553d60a61333c2e08306dc4b5aec0cf9127
inamuu <8310973+inamuu@users.noreply.github.com>
2020-03-16 15:34:17 +0900

    :hammer: ProfileをLinuxでも共通で使えるように変更

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  profile = "XXXXXXXXXXX"
+  region  = "${var.aws_region}"
 }
 
 terraform {
@@ -9,9 +8,6 @@ terraform {
     bucket                  = "${var.backend_bucket_name}"
     key                     = "terraform.tfstate"
     region                  = "${var.aws_region}"
-    shared_credentials_file = "~/.aws/credentials"
-
-    ### profile名は変数で定義してもこけるので直接記述する
     profile = "XXXXXXXXXXX"
   }
 }

1255b4f316a7b12b921041f785209344a6b17ace
inamuu <8310973+inamuu@users.noreply.github.com>
2020-06-10 17:46:01 +0900

    :cop: 汎用的なネットワーク範囲へ変更

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -2,7 +2,7 @@
 # example
 resource "aws_subnet" "example-public-1a" {
   vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.50.0.0/26"
+  cidr_block        = "10.0.0.0/26"
   availability_zone = "ap-northeast-1a"
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "example-public-1a" {
 
 resource "aws_subnet" "example-public-1c" {
   vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.50.0.64/26"
+  cidr_block        = "10.0.0.64/26"
   availability_zone = "ap-northeast-1c"
 
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "example-public-1c" {
 
 resource "aws_subnet" "example-private-1a" {
   vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.50.0.128/26"
+  cidr_block        = "10.0.0.128/26"
   availability_zone = "ap-northeast-1a"
 
   tags = {
@@ -32,7 +32,7 @@ resource "aws_subnet" "example-private-1a" {
 
 resource "aws_subnet" "example-private-1c" {
   vpc_id            = aws_vpc.example.id
-  cidr_block        = "10.50.0.192/26"
+  cidr_block        = "10.0.0.192/26"
   availability_zone = "ap-northeast-1c"
 
   tags = {

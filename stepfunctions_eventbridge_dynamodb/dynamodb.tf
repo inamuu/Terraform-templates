e172159a494be02140e5738f6f8fa96b54b4ae28
inamuu <8310973+inamuu@users.noreply.github.com>
2023-02-27 20:00:06 +0900

    add dynamodb table

--- stepfunctions_eventbridge_dynamodb/dynamodb.tf
+++ stepfunctions_eventbridge_dynamodb/dynamodb.tf
@@ -1,13 +1,10 @@
-#resource "aws_dynamodb_table" "" {
-#  name         = "example-terraform-state-lock"
-#  billing_mode = "PAY_PER_REQUEST"
-#  hash_key     = "eventId"
-#  attribute {
-#    name = "LockID"
-#    type = "S"
-#  }
-#  ttl {
-#    attribute_name = "TTL"
-#    enabled        = true
-#  }
-#}
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name         = "${var.default_prefix}-${random_id.specify_id.hex}-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "eventId"
+
+  attribute {
+    name = "eventId"
+    type = "S"
+  }
+}

25d2e2ce9a01f31875b927d7f96ef95e652d78f4
inamuu <8310973+inamuu@users.noreply.github.com>
2023-03-01 13:49:06 +0900

    add iam lines to sfn

--- stepfunctions_eventbridge_dynamodb/sfn_state_machine.tf
+++ stepfunctions_eventbridge_dynamodb/sfn_state_machine.tf
@@ -1,3 +1,35 @@
+resource "aws_iam_role" "stepfunctions" {
+  name = "${var.default_prefix}-${random_id.specify_id.hex}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "states.ap-northeast-1.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "MyStepFunctionsPolicy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "dynamodb:PutItem"
+          Resource = aws_dynamodb_table.dynamodb_table.arn
+        }
+      ]
+    })
+  }
+}
+
 resource "aws_sfn_state_machine" "state_machine" {
   name     = "${var.default_prefix}-${random_id.specify_id.hex}"
   role_arn = aws_iam_role.stepfunctions.arn

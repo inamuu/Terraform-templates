2b9d49819578d0a3190ed1eae14a2364bbe7d9ad
inamuu <8310973+inamuu@users.noreply.github.com>
2023-03-02 17:54:29 +0900

    add a sample lambda

--- stepfunctions_eventbridge_dynamodb/lambda.tf
+++ stepfunctions_eventbridge_dynamodb/lambda.tf
@@ -46,8 +46,9 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
 }
 
+### Slack Notify
 resource "aws_lambda_function" "lambda_slack" {
-  filename         = "lambda/zip/lambda.zip"
+  filename         = "lambda/slack_notify/zip/lambda.zip"
   function_name    = "${var.default_prefix}-${random_id.specify_id.hex}-slack-notify"
   role             = aws_iam_role.lambda_role.arn
   handler          = "main.lambda_handler"
@@ -64,6 +65,29 @@ resource "aws_lambda_function" "lambda_slack" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "lambda/src/"
-  output_path = "lambda/zip/lambda.zip"
+  source_dir  = "lambda/slack_notify/src/"
+  output_path = "lambda/slack_notify/zip/lambda.zip"
+}
+
+### Preprocess
+resource "aws_lambda_function" "lambda_preprocess" {
+  filename         = "lambda/preprocess/zip/lambda.zip"
+  function_name    = "${var.default_prefix}-${random_id.specify_id.hex}-preprocess"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime          = "python3.9"
+  timeout          = 30
+
+  environment {
+    variables = {
+      slack_webhook_url = var.slack_webhook_url
+    }
+  }
+}
+
+data "archive_file" "lambda_zip_preprocess" {
+  type        = "zip"
+  source_dir  = "lambda/preprocess/src/"
+  output_path = "lambda/preprocess/zip/lambda.zip"
 }
